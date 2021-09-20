a8b8873981900d668e9bab1cd9bf4b805992d41e
Elena <rhapsodyenmauve@gmail.com>
2021-09-20 23:47:48 +0100

    hover properties on menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,13 @@ import './menu-item.styles.scss';
 
 // destructuring the title - it's equal to writing props.title
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    ></div>
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
