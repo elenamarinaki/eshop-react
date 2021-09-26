0a8a038a67a95833279bf76763e5a6173ef3a5cd
Elena <rhapsodyenmauve@gmail.com>
2021-09-26 01:13:54 +0100

    structuring the collection preview component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './collection-preview.styles.scss';
+
+const CollectionPreview = () => (
+  <div className='collection-preview'>
+    <h1 className='title'>Title</h1>
+    <div className='preview'></div>
+  </div>
+);
