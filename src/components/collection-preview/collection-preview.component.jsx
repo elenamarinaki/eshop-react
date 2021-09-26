fefbd321dc8fa85998306158d237d2ae93d41e31
Elena <rhapsodyenmauve@gmail.com>
2021-09-26 01:31:12 +0100

    passing props from the shop component to the collection-preview component the /shop page now shows the a list of items for every collection

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = () => (
+const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='title'>Title</h1>
-    <div className='preview'></div>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
   </div>
 );
+
+export default CollectionPreview;
